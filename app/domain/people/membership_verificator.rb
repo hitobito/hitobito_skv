368c3093bc214e5b077709fd668be927490f1217
Mountain Star <github@mtnstar.net>
2021-12-15 07:55:53 +0100

    Add verificator, add logo/address, ... to view

---
+++ app/domain/people/membership_verificator.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2021, Schweizerischer Kanu-Verband. This file is part of
+#  hitobito_skv and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_skv.
+
+class People::MembershipVerificator
+
+  def initialize(person)
+    @person = person
+  end
+
+  def member?
+    @person.roles.any? do |r|
+      r.kind != :external
+    end
+  end
+
+end
---
+++ spec/domain/people/membership_verificator_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2021, Schweizerischer Kanu-Verband. This file is part of
+#  hitobito_skv and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_skv.
+
+require 'spec_helper'
+
+describe People::MembershipVerificator do
+
+  let(:verificator) { People::MembershipVerificator.new(person) }
+  let(:person) { people(:berner_kanufahrer) }
+  let!(:external_group) { Group::ExterneKontakte.create!(parent: Group.root, name: 'Extis') }
+
+  context '#member?' do
+    it 'returns true if person has member role' do
+      expect(verificator.member?).to eq(true)
+    end
+
+    it 'returns true if person has any non external role' do
+      Fabricate(Group::ExterneKontakte::ExternerKontakt.name.to_sym,
+                group: external_group, person: person)
+
+      expect(verificator.member?).to eq(true)
+    end
+
+    it 'returns false if person has no role' do
+      person.roles.destroy_all
+
+      expect(verificator.member?).to eq(true)
+    end
+
+    it 'returns false if person has only external role' do
+      person.roles.destroy_all
+      Fabricate(Group::ExterneKontakte::ExternerKontakt.name.to_sym,
+                group: external_group, person: person)
+
+      expect(verificator.member?).to eq(false)
+    end
+  end
+
+end
