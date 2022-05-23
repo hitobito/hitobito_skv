829b147ff31338a1834fc4d5cf087f5a6fdb70e2
Carlo Beltrame <beltrame@puzzle.ch>
2022-05-23 11:31:46 +0200

    Membership token should be an internal attribute
    Refs hitobito/hitobito#1744

--- app/models/skv/person.rb
+++ app/models/skv/person.rb
@@ -10,6 +10,8 @@ module Skv::Person
 
   included do
     validates :membership_verify_token, uniqueness: { allow_blank: true }
+    
+    Person::INTERNAL_ATTRS << :membership_verify_token
   end
 
   def init_membership_verify_token!
